b647aea1a6fa8c3b152acf0216ce304e364fd475
Akash Shrestha <akashshrestha0399@gmail.com>
2024-10-08 15:01:40 +0530

    colors added

--- src/components/ProductEdit/ProductEditor.jsx
+++ src/components/ProductEdit/ProductEditor.jsx
@@ -11,90 +11,64 @@ const ProductEditor = ({ product }) => {
 
   const handleShortlist = () => {
     setIsShortlisted(true);
-    dispatch(shortlist(product))
+    dispatch(shortlist(product));
   };
+
+  // Parse the color array from the stringified format
+
+  
+  const colors = product?.colour ? JSON.parse(product.colour) : [];
+
   return (
-    <div class=" Product-editor p-3 ">
-      <div class=" edit-container-info-section d-flex justify-content-between align-items-center">
-        <div class="m-4" style={{ width: "80%" }}>
-          <h2 class="cart-details-heading">{product?.productName}</h2>
-          <p class="cart-details-text">{product?.productDescription}</p>
-          <div className="price mt-2 ">
-            {" "}
+    <div className="Product-editor p-3">
+      <div className="edit-container-info-section d-flex justify-content-between align-items-center">
+        <div className="m-4" style={{ width: "80%" }}>
+          <h2 className="cart-details-heading">{product?.productName}</h2>
+          <p className="cart-details-text">{product?.productDescription}</p>
+          <div className="price mt-2">
             <s>₹{product?.productMRP}</s>₹{product?.productPrice}
           </div>
         </div>
-        <div class="btn-sep-cart-details">
-          <button class="btn bg-pink" onClick={handleShortlist}>
+        <div className="btn-sep-cart-details">
+          <button className="btn bg-pink" onClick={handleShortlist}>
             {isShortlisted ? "Shortlisted" : "Shortlist"}
           </button>
-
-          {/* <Link href="/cart">
-            <button class="btn bg-sky-blue">Add To Cart</button>
-          </Link> */}
         </div>
       </div>
 
       <hr />
-      <div class="choose-color-container  m-4">
-        <h5 class="">
-          <span class="product-caption">
+      <div className="choose-color-container m-4">
+        <h5>
+          <span className="product-caption">
             <b>Choose your colors</b>
           </span>
         </h5>
-        <div class="color-select d-flex pt-3 pl-4">
-          <button class="circle-border">
-            <div class="p-circle" style={{ backgroundColor: "red" }}></div>
-          </button>
-          <button class="circle-border">
-            <div
-              class="p-circle"
-              style={{ backgroundColor: "rgb(239, 176, 18)" }}
-            ></div>
-          </button>
-          <button class="circle-border">
-            <div
-              class="p-circle"
-              style={{ backgroundColor: "rgb(255, 39, 244)" }}
-            ></div>
-          </button>
-          <button class="circle-border">
-            <div
-              class="p-circle"
-              style={{ backgroundColor: "rgb(46, 68, 239)" }}
-            ></div>
-          </button>
+        <div className="color-select d-flex pt-3 pl-4">
+          {colors?.length > 0 ? (
+            colors.map((color, index) => (
+              <button className="circle-border" key={index}>
+                <div
+                  className="p-circle"
+                  style={{ backgroundColor: color }}
+                ></div>
+              </button>
+            ))
+          ) : (
+            <p>No Colours Available</p>
+          )}
         </div>
       </div>
       <hr />
-      <div class=" offer-card uplaod-img-section m-4">
-        <h5 class="">
+      <div className="offer-card uplaod-img-section m-4">
+        <h5>
           &nbsp;
-          <span class="product-caption">
+          <span className="product-caption">
             <b>Descriptions</b>
           </span>
         </h5>
 
         <ul>
-          <li> {product?.productDescription}</li>
-          {/* <li>
-            Bank Offer: Get ₹50 instant discount on first UPI txn on order of
-            ₹200 and aboveT&C{" "}
-          </li>
-
-          <li> Bank Offer: 5% Cashback on Axis Bank CardT&C </li>
-
-          <li>
-            {" "}
-            Bank Offer: 10% off up to ₹1,250 on HDFC Bank Credit Card EMI Txns,
-            Tenure: 6 and 9 months, Min Txn Value: ₹7500T&C{" "}
-          </li>
-
-          <li>
-            {" "}
-            Special Price: Get extra 10% off (price inclusive of
-            cashback/coupon)T&C
-          </li> */}
+          <li>{product?.productDescription}</li>
         </ul>
       </div>
     </div>
